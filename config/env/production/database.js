4cc174cd65a254cf7133a8499b04361292fc6bef
JonesTPG <joonas.ryynanen1@gmail.com>
2020-06-16 15:21:53 +0300

    envinronment config templates.

---
+++ config/env/production/database.js
@@ -0,0 +1,15 @@
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "mongoose",
+      settings: {
+        uri: env("DATABASE_URI"),
+      },
+      options: {
+        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+        ssl: env.bool("DATABASE_SSL", true),
+      },
+    },
+  },
+});
