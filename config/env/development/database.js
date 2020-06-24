67af0a1d8774e1d1561c739445439c3cf61f91ff
JonesTPG <joonas.ryynanen1@gmail.com>
2020-06-24 12:15:41 +0300

    dev environment config changes.

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -4,11 +4,16 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        uri: env('DEV_DATABASE_URI'),
+        client: 'mongo',
+        host: env('DEV_DATABASE_HOST', 'localhost'),
+        port: env.int('DEV_DATABASE_PORT', 27017),
+        database: env('DEV_DATABASE_NAME', 'strapi'),
+        username: env('DEV_DATABASE_USERNAME', 'strapi'),
+        password: env('DEV_DATABASE_PASSWORD', 'strapi'),
       },
       options: {
         authenticationDatabase: env('DEV_AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DEV_DATABASE_SSL', true),
+        ssl: env('DEV_DATABASE_SSL', true),
       },
     },
   },
